864735c50c4d9b367bad99978a78e6e92036f234
mansoor964 <mansoor.ahmadkth@gmail.com>
2025-02-24 19:01:44 +0500

    functional programming

---
+++ decorators.py
@@ -0,0 +1,13 @@
+# Functional Programming:
+def announce(f):
+    def wrapper():
+        print('about to run the function ..')
+        f()
+        print('done with the function.')
+    return wrapper
+
+@announce
+def hello():
+    print('hello world!')
+
+hello()
