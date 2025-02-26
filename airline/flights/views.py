e0a9019ff397f829f854e1f6bacb51d2942425db
mansoor964 <mansoor.ahmadkth@gmail.com>
2025-02-27 04:06:12 +0500

    'progress'

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Flight
 
 # Create your views here.
+def index(request):
+    return render(request, 'flights/index.html',{
+        'flights': Flight.objects.all()
+    })
